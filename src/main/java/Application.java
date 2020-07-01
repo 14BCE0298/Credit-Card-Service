fd6c71d4be5229dde672e262465907691d2e3149
Vishal Sharma <vvvishal35@gmail.com>
2020-07-01 09:41:43 +0530

    Adding required SpringBoot dependencies and declaring springboot application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,9 @@
 import models.CreditCard;
 import models.Customer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Application {
     //TODO : Add API to apply for credit card
     //TODO : Add API to make purchase
@@ -9,15 +12,16 @@ public class Application {
     //TODO : Add API to get total due amount and date
     //TODO : Add API to get all transactions
     public static void main(String[] args) {
-        Customer customer = new Customer();
-        customer.setAge(24);
-        customer.setName("Vishal Sharma");
-        customer.setEmail("vishal.sharma@email.com");
-        customer.setPhone("9876543210");
-        customer.setUserId(customer.createUserId());
-
-        CreditCard creditCard = new CreditCard();
-        creditCard.setCardOwner(customer);
-        creditCard.getCardMonthlyLimit();
+        SpringApplication.run(Application.class, args);
+//        Customer customer = new Customer();
+//        customer.setAge(24);
+//        customer.setName("Vishal Sharma");
+//        customer.setEmail("vishal.sharma@email.com");
+//        customer.setPhone("9876543210");
+//        customer.setUserId(customer.createUserId());
+//
+//        CreditCard creditCard = new CreditCard();
+//        creditCard.setCardOwner(customer);
+//        creditCard.getCardMonthlyLimit();
     }
 }
