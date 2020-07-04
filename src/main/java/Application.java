3c1e47c09d21b719a746ec2b7d8fb79da3ca6cf4
Vishal Sharma <vvvishal35@gmail.com>
2020-07-05 00:50:35 +0530

    Refactoring main class

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,30 +1,22 @@
+import models.CardType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+//TODO : Reading data from yml config file
+//TODO : Add API to list type of credit cards
+//TODO : Add API to apply for credit card
+//TODO : Add API to make purchase
+//TODO : Add API to return current amount due
+//TODO : Add API to deposit due amount
+//TODO : Add API to get total due amount and date
+//TODO : Add API to get all transactions
+//TODO : Send purchase API data to a kafka broker
+
 @SpringBootApplication
 @ComponentScan(basePackages = "java")
 public class Application {
-    //TODO : Reading data from yml config file
-    //TODO : Add API to list type of credit cards
-    //TODO : Add API to apply for credit card
-    //TODO : Add API to make purchase
-    //TODO : Add API to return current amount due
-    //TODO : Add API to deposit due amount
-    //TODO : Add API to get total due amount and date
-    //TODO : Add API to get all transactions
-    //TODO : Send purchase API data to a kafka broker
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-//        Customer customer = new Customer();
-//        customer.setAge(24);
-//        customer.setName("Vishal Sharma");
-//        customer.setEmail("vishal.sharma@email.com");
-//        customer.setPhone("9876543210");
-//        customer.setUserId(customer.createUserId());
-//
-//        CreditCard creditCard = new CreditCard();
-//        creditCard.setCardOwner(customer);
-//        creditCard.getCardMonthlyLimit();
     }
 }
