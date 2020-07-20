4bf1d72cdc7f48a9afc5dcc17a7d8ee0a70bc679
Vishal Sharma <vvvishal35@gmail.com>
2020-07-20 12:12:50 +0530

    Updating TODO items

--- src/main/java/com/project/ccs/Application.java
+++ src/main/java/com/project/ccs/Application.java
@@ -4,15 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-//TODO : Read file for setting users to flagged
+//TODO : Read file using camel for setting users to flagged
 //TODO : Send new User data to file using camel
-//TODO : Add API to apply for credit card
 //TODO : Add API to make purchase
 //TODO : Add API to deposit due amount
-//TODO : Add API to get total due amount and date
-//TODO : Add API to get all transactions
+//TODO : Add API to get all transactions and due amount
 //TODO: About API using service details class with git branch details
-//TODO: Add constraints on user input
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.project.ccs")
