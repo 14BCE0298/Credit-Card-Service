8c44570ad10d27c2e68655464e8fe44ae83223c4
Vishal Sharma <vvvishal35@gmail.com>
2020-07-20 11:30:23 +0530

    Formatting constraint violation error message

--- src/main/java/com/project/ccs/customExceptions/ExceptionController.java
+++ src/main/java/com/project/ccs/customExceptions/ExceptionController.java
@@ -5,6 +5,7 @@ import com.project.ccs.customExceptions.exceptions.UserNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,10 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.validation.ConstraintViolationException;
+
 @ControllerAdvice
 @RestController
 public class ExceptionController extends ResponseEntityExceptionHandler {
 
+    private static final String CONSTRAINT_VIOLATION_ERROR = "%s is violated as value %s";
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException exception) {
         return new ResponseEntity<>(exception.toString(), HttpStatus.NOT_FOUND);
@@ -28,6 +32,11 @@ public class ExceptionController extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
                                                                   HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return new ResponseEntity<>(exception.getBindingResult().toString(), HttpStatus.BAD_REQUEST);
+        String message = String.format(CONSTRAINT_VIOLATION_ERROR,
+                exception.getBindingResult().getAllErrors().get(0).getCodes()[0],
+                exception.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+
+
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
     }
 }
